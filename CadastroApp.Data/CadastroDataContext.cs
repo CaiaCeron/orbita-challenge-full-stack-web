705e0d0ab387ea2a7ea0c72ccaeff63f248e7fc5
caiaceron <caiaceron@gmail.com>
2022-07-06 12:23:14 -0300

    api e frontend

---
+++ CadastroApp.Data/CadastroDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroApp.Data
+{
+    public class CadastroDataContext : DbContext
+    {
+        public CadastroDataContext(DbContextOptions<CadastroDataContext> options) 
+            :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ apiCadastro/ViewModels/StudentView.cs
@@ -0,0 +1,14 @@
+﻿namespace apiCadastro.ViewModels
+{
+    public class StudentView
+    {
+       
+        public string AcademicRegistry { get; set; }
+        
+        public string Name { get; set; }
+    
+        public string Cpf { get; set; }
+
+        public string Email { get; set; }
+    }
+}
