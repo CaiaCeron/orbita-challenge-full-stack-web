8656bdf2c94baeb343c39db51b64eaffca703a95
caiaceron <caiaceron@gmail.com>
2022-07-05 22:03:37 -0300

    reset repo2

--- fronte_end/src/plugins/vuetify.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import '@mdi/font/css/materialdesignicons.css';
-
-Vue.use(Vuetify);
-
-export default new Vuetify({});
